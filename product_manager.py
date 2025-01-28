4f66f702688bcc17a2a4aa9e953069e1e0c368dc
Daisydi216 <daisydi21643@gmail.com>
2025-01-28 16:31:44 +0100

    Added product_manager.py file

---
+++ product_manager.py
@@ -0,0 +1,19 @@
+from product import Product
+
+class ProductManager:
+    def __init__(self):
+        self._products = []
+        
+    def add_product(self, product):
+        if isinstance(product, Product):
+            self._products.append(product)
+        else:
+            print("Invalid product.")
+            
+    def view_all_products(self):
+        for product in self._products:
+            print(product.display_info())
+            
+    def total_value(self):
+        total = sum(p._price * p._quantity for p in self._products)
+        print(f"Total Inventory Value: {total}")
