6aa62c180f209f7238640ce332c57d622c7f3d58
Daisydi216 <daisydi21643@gmail.com>
2025-01-28 16:44:01 +0100

    Changed the names of the products that were added and their quantity, Deleted lines of code related to product display and total inventory value.

--- main.py
+++ main.py
@@ -6,13 +6,11 @@ product_manager = ProductManager()
 
 
 products = [
-    Product("Laptop Asus TUF", 2.150, 25),
-    Product("Samsung Galaxy s25 Ultra", 1.502, 5),
-    Product("LG TV", 400, 4)
+    Product("Laptop", 2.150, 15),
+    Product("Samsung Galaxy s23 Ultra", 1.502, 15),
+    Product("Monitor", 400, 14)
 ]
 
 for product in products:
      product_manager.add_product(product)
      
-product_manager.view_all_products()
-product_manager.total_value()
